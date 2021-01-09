e45e2ea483689442186bd5ded46c0f3899f7ef32
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-01-09 20:39:37 +0100

    Сделал и добавил Репетины ДЗ 4

--- js/hw3.js
+++ js/hw3.js
@@ -1,13 +1,13 @@
-/* const ADMIN_PASSWORD = 'jqueryismyjam'; */
-//let message;
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
 let password = prompt('write your password')
 
-/* if (password === ADMIN_PASSWORD) {
+ if (password === ADMIN_PASSWORD) {
     message = 'Добро пожаловать!'
 } else if (password === null) {
     message = 'Отменено пользователем!'
 } else {
      message = 'Доступ запрещен, неверный пароль!'
 }
- */
-//alert(message)
+ 
+alert(message)
